a797fe5450e3a72b8ee6d59e08a9b6335b9c4c38
Maria Arias de Reyna Dominguez <marias@emergya.com>
2011-04-08 14:12:54 +0200

    Pregenerating Routes now with a different table for the target/source points

--- core/src/main/resources/pregenerate_routing.sql
+++ core/src/main/resources/pregenerate_routing.sql
@@ -1,13 +1,15 @@
 ﻿DROP FUNCTION IF EXISTS _pregenera_routing(TEXT, integer, integer);
 
-CREATE FUNCTION _pregenera_routing(routing_table TEXT,  source integer, target integer)
+--"private" function which generates the routing cache 
+--from source to target using routing_table
+CREATE FUNCTION _pregenera_routing(routing_table TEXT,  s integer, t integer)
 RETURNS BOOLEAN AS
 $BODY$
         BEGIN
 		BEGIN
 		INSERT INTO routing_pregenerated
-			(vertex_id, edge_id, "cost")
-			(SELECT vertex_id, edge_id, "cost"
+			(source, target, vertex_id, edge_id, "cost")
+			(SELECT s as source, t as target, vertex_id, edge_id, "cost"
 				from shortest_path_shooting_star( 
 			'select id as id, 
 				source::integer, 
@@ -21,34 +23,38 @@ $BODY$
 				st_length(the_geom)::double precision as to_cost,
 				st_length(the_geom)::double precision as reverse_cost
 				from ' || routing_table,
-			   source,
-			   target,
+			   s,
+			   t,
 			   true,
 			   true));
 		EXCEPTION 
 			WHEN internal_error THEN 
-			RAISE WARNING 'Error calculating route from (%) to (%)',source,target;
+			RAISE WARNING 'Error calculating route from (%) to (%)',s,t;
 		END;
 	RETURN TRUE;
         END;
 $BODY$ 
 LANGUAGE 'plpgsql';
 
-DROP FUNCTION IF EXISTS pregenera_routing(TEXT, TEXT);
+DROP FUNCTION IF EXISTS pregenera_routing(TEXT, TEXT, TEXT);
 
-CREATE FUNCTION pregenera_routing(rtable TEXT, gid TEXT)
+--Routing table, Points table, ID column
+--The points table contains a reference to gid(routing) from/to
+--which we want to pregenerate routing
+--Routing table contains all the pgrouting data
+CREATE FUNCTION pregenera_routing(rtable TEXT, ptable TEXT, gid TEXT)
 RETURNS BOOLEAN AS 
 $BODY$
-	DECLARE
-		_row RECORD;
         BEGIN
             EXECUTE 'DROP TABLE IF EXISTS ' || rtable || '_pregenerated';
 	    EXECUTE 'CREATE TABLE ' || rtable || '_pregenerated
 			(
 			  id bigserial NOT NULL,
-			  vertex_id INTEGER,
-			  edge_id INTEGER,
-			  "cost" DOUBLE PRECISION,
+			  source INTEGER NOT NULL,
+			  target INTEGER NOT NULL,
+			  vertex_id INTEGER NOT NULL,
+			  edge_id INTEGER NOT NULL,
+			  "cost" DOUBLE PRECISION NOT NULL,
 			  CONSTRAINT routing_pregenerated_key PRIMARY KEY (id)
 			)';
 
@@ -57,7 +63,7 @@ $BODY$
             EXECUTE 'SELECT _pregenera_routing(''' || rtable || ''' :: TEXT, 
 						s.' || gid || ' :: INTEGER, 
 						t.' || gid || ' :: INTEGER)
-		    FROM ' || rtable || ' AS s CROSS JOIN ' || rtable || ' AS t 
+		    FROM ' || ptable || ' AS s CROSS JOIN ' || ptable || ' AS t 
 		    WHERE s.' || gid || ' <> t.' || gid || '';
 	    RAISE NOTICE 'Pregenerated process completed sucessfully.';   
 	    RETURN TRUE; 
